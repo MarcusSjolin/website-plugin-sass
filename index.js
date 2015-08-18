d8d92e20c68304af6abda05598b89dcf086762fb
Marcus Sjolin <marcussjolin@icloud.com>
2015-08-18 22:58:51 +0100

    Reverting react and adding scss back in, silly mistake

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 var path = require("path")
 exports = module.exports = function websitePluginSass(website, next) {
     website.webpack.module.loaders.push ({
-        test: /\.js$/,
-        loader: process.cwd() + "/node_modules/website-plugin-react/node_modules/jsx-loader"
-    })
-    website.webpack.module.loaders.push ({
-        test: /\.jsx$/,
-        loader: process.cwd() + "/node_modules/website-plugin-react/node_modules/jsx-loader?insertPragma=React.DOM"
+        test: /\.scss$/,
+        loader: "../node_modules/website-plugin-sass/node_modules/style-loader!../node_modules/website-plugin-sass/node_modules/css-loader!../node_modules/website-plugin-sass/node_modules/sass-loader"
     })
     next()
 }
