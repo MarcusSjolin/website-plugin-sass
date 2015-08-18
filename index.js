253b95fb6df08d850655f9cef260d7200dbd8225
Marcus Sjolin <marcussjolin@icloud.com>
2015-08-18 22:47:34 +0100

    Added index file

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+var path = require("path")
 exports = module.exports = function websitePluginSass(website, next) {
-    website.webpack.modules = require("sass-loader")
-    next(website)
+    website.webpack.module.loaders.push ({
+        test: /\.scss$/,
+        loader: "../node_modules/website-plugin-sass/node_modules/style-loader!../node_modules/website-plugin-sass/node_modules/css-loader!../node_modules/website-plugin-sass/node_modules/sass-loader"
+    })
+    next()
 }
